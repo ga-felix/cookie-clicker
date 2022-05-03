0bb6779eb0858fbb3a93017bc42b1b8e86af2e22
ga-felix <gafelix435@gmail.com>
2022-05-02 21:06:03 -0300

    feat(clicker): add first clickable cookie

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './cookie/Cookie.js'
+import Cookie from './cookie/Cookie.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Cookie width="250" height="250"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cookie/Cookie.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './Cookie.css'
+
+function Cookie(props) {
+
+    const [count, setCount] = useState(0)
+
+    function increment() {
+        setCount(count + 1)
+    }
+
+    function clickCookie() {
+        increment()
+    }
+
+    return (
+        <div id="superCookie!">
+            <h1>You've clicked {count} times!</h1>,
+            <img onClick={clickCookie} onAnimationEnd={console.log("end")} src="/cookie.png" width={props.width} height={props.height} alt="The big cookie" class="center"></img>
+        </div>
+    );
+
+}
+
+export default Cookie;
